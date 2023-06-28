c9be3b1abff9c376c71140a32883325be11b3f46
nikhil1610 <nikhilgautam106@gmail.com>
2023-06-28 21:33:12 +0530

    about section complete

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,9 +26,11 @@ const Hero = () => {
       <ComputersCanvas/>
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center '>
-        <a href='#about'>
+        <a href='#about' 
+        // className='translate-y-16 cusror-pointer'
+        >
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary
-          flex justify-center items-start p-2 translate-y-16'>
+          flex justify-center items-start p-2 '>
             <motion.div
             animate={{
               y:[0,24,0]
